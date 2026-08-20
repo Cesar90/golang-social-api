1153e3d54f5f39aac24ec46d09d8cc304421b7d9
Cesar <ccordero.007@gmail.com>
2026-08-19 21:04:04 -0600

    Implementing the Repository Pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/Cesar90/golang-social-api/internal/env"
+	"github.com/Cesar90/golang-social-api/internal/store"
 )
 
 func main() {
@@ -12,8 +13,11 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
