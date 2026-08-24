61f7a97aaa8c791a27d54cd30be33c63b4864e8e
Cesar <ccordero.007@gmail.com>
2026-08-24 15:20:44 -0600

    Marshalling JSON responses

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,7 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok 2"))
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write([]byte(`{"status": "OK"}`))
+	data := map[string]string{
+		"status":  "ok",
+		"env":     app.config.env,
+		"version": version,
+	}
+
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		// log.Print(err.Error())
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+	}
 }
