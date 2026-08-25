b56eecfcb296d76849e32c9141b722138a8a76c2
Cesar <ccordero.007@gmail.com>
2026-08-24 22:30:56 -0600

    Updating and Deleting Posts

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"log"
 	"net/http"
@@ -9,6 +10,10 @@ import (
 	"github.com/Cesar90/golang-social-api/internal/store"
 	"github.com/go-chi/chi/v5"
 )
+
+type postKey string
+
+const postCtx postKey = "post"
 
 type CreatePostPayload struct {
 	Title   string   `json:"title" validate:"required,max=100"`
@@ -55,29 +60,35 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "postID")
-	id, err := strconv.ParseInt(idParam, 10, 64)
-	if err != nil {
-		// writeJSONError(w, http.StatusInternalServerError, err.Error())
+	// idParam := chi.URLParam(r, "postID")
+	// id, err := strconv.ParseInt(idParam, 10, 64)
+	// if err != nil {
+	// 	// writeJSONError(w, http.StatusInternalServerError, err.Error())
+	// 	app.internalServerError(w, r, err)
+	// 	return
+	// }
+
+	// ctx := r.Context()
+	// post, err := app.store.Posts.GetByID(ctx, id)
+	// if err != nil {
+	// 	switch {
+	// 	case errors.Is(err, store.ErrNotFound):
+	// 		// writeJSONError(w, http.StatusNotFound, err.Error())
+	// 		app.notFoundResponse(w, r, err)
+	// 	default:
+	// 		// writeJSONError(w, http.StatusInternalServerError, err.Error())
+	// 		app.internalServerError(w, r, err)
+	// 	}
+	// 	return
+	// }
+
+	post := getPostFromCtx(r)
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
 
-	ctx := r.Context()
-	post, err := app.store.Posts.GetByID(ctx, id)
-	if err != nil {
-		switch {
-		case errors.Is(err, store.ErrNotFound):
-			// writeJSONError(w, http.StatusNotFound, err.Error())
-			app.notFoundResponse(w, r, err)
-		default:
-			// writeJSONError(w, http.StatusInternalServerError, err.Error())
-			app.internalServerError(w, r, err)
-		}
-		return
-	}
-
-	comments, err := app.store.Comments.GetByPostID(ctx, id)
+	comments, err := app.store.Comments.GetByPostID(r.Context(), post.ID)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
@@ -90,4 +101,111 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 		app.internalServerError(w, r, err)
 		return
 	}
+}
+
+func (app *application) deletePostHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		// writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.store.Posts.Delete(ctx, id); err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			app.notFoundResponse(w, r, err)
+		default:
+			app.internalServerError(w, r, err)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+/*
+	Use pointers so we can distinguish between a field that was omitted
+	from the request and a field that was explicitly provided.
+
+	For example:
+	  { "title": "title" }
+
+	In this case, Content is nil, meaning the client did not provide
+	the content field. A non-nil Content indicates that the field was
+	present in the request, even if its value is an empty string.
+*/
+
+type UpdatePostPayload struct {
+	Title   *string `json:"title" validate:"omitempty,max=100"`
+	Content *string `json:"content" validate:"omitempty,max=1000"`
+}
+
+func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request) {
+	post := getPostFromCtx(r)
+
+	var payload UpdatePostPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if payload.Content != nil {
+		post.Content = *payload.Content
+	}
+
+	if payload.Title != nil {
+		post.Title = *payload.Title
+	}
+
+	if err := app.store.Posts.Update(r.Context(), post); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) postsContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "postID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			// writeJSONError(w, http.StatusInternalServerError, err.Error())
+			app.internalServerError(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+		post, err := app.store.Posts.GetByID(ctx, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				// writeJSONError(w, http.StatusNotFound, err.Error())
+				app.notFoundResponse(w, r, err)
+			default:
+				// writeJSONError(w, http.StatusInternalServerError, err.Error())
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+
+		ctx = context.WithValue(ctx, postCtx, post)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getPostFromCtx(r *http.Request) *store.Post {
+	post, _ := r.Context().Value(postCtx).(*store.Post)
+	return post
 }
