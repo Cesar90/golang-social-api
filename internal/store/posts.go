77098b0bcb22dfb090d77c8aeaa8da98ac84df42
Cesar <ccordero.007@gmail.com>
2026-08-25 16:03:24 -0600

    Optimistic concurrency control

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -17,6 +17,7 @@ type Post struct {
 	Tags      []string  `json:"tags"`
 	CreatedAt string    `json:"created_at"`
 	UpdatedAt string    `json:"updated_at"`
+	Version   int       `json:"version"`
 	Comments  []Comment `json:"comments"`
 }
 
@@ -57,7 +58,7 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 
 func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 	query := `
-		SELECT Id, user_id, title, content, created_at, tags, updated_at
+		SELECT Id, user_id, title, content, created_at, tags, updated_at, version
 		FROM posts
 		WHERE id = $1
 	`
@@ -71,6 +72,7 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 		&post.CreatedAt,
 		pq.Array(&post.Tags),
 		&post.UpdatedAt,
+		&post.Version,
 	)
 
 	if err != nil {
@@ -107,12 +109,29 @@ func (s *PostStore) Delete(ctx context.Context, postID int64) error {
 func (s *PostStore) Update(ctx context.Context, post *Post) error {
 	query := `
 		UPDATE posts
-		SET title = $1, content = $2
-		WHERE id = $3
+		SET title = $1,
+			content = $2,
+			version = version + 1
+		WHERE id = $3 AND version = $4
+		RETURNING version
 	`
-	_, err := s.db.ExecContext(ctx, query, post.Title, post.Content, post.ID)
+	// _, err := s.db.ExecContext(ctx, query, post.Title, post.Content, post.ID, post.Version)
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Title,
+		post.Content,
+		post.ID,
+		post.Version,
+	).Scan(&post.Version)
 	if err != nil {
-		return err
+		// return err
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrNotFound
+		default:
+			return err
+		}
 	}
 
 	return nil
