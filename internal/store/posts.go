ca76d1bd3e00adedc44e5640bca7fa8d9f886fa3
Cesar <ccordero.007@gmail.com>
2026-08-26 17:45:33 -0600

    Pagination and Sorting

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -27,7 +27,7 @@ type PostWithMedata struct {
 	CommentCount int `json:"comments_count"`
 }
 
-func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMedata, error) {
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginationFeedQuery) ([]PostWithMedata, error) {
 	query := `
 		select 
 			p.id, p.user_id, p.title, p."content" , p.created_at , p."version" , p.tags ,
@@ -39,12 +39,13 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMe
 		join followers f on f.follower_id = p.user_id or p.user_id = $1
 		where f.user_id = $1 or p.user_id = $1
 		group by p.id, u.username
-		order by p.created_at desc
+		order by p.created_at ` + fq.Sort + `
+		LIMIT $2 OFFSET $3
 	`
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
 	if err != nil {
 		return nil, err
 	}
